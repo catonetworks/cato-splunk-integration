cf0081383ca529029c6ff1623f9544c0c03be7c4
Brian Anderson <brian.anderson@catonetworks.com>
2025-04-07 21:21:42 -0700

    Updated eventsFeed.sh with localhost:8000

--- scripts/eventsFeed.sh
+++ scripts/eventsFeed.sh
@@ -5,4 +5,4 @@ CATO_TOKEN="${CATO_TOKEN}"
 CATO_ACCOUNT_ID="${CATO_ACCOUNT_ID}"
 
 # Run the Python script with environment variables
-/usr/bin/python /opt/splunk/bin/scripts/eventsFeed.py -K "$CATO_TOKEN" -I "$CATO_ACCOUNT_ID" -c "/tmp/config.txt" -p 
+python3 eventsFeed.py -K "$CATO_TOKEN" -I "$CATO_ACCOUNT_ID" -pP -n localhost:1514 
